683ce1e1481f82747bb3240e17c4f551694b6b73
yashar <yashar>
2025-10-23 14:57:03 +0000

    add new field to database

--- qweb_test/models/extended_sale.py
+++ qweb_test/models/extended_sale.py
@@ -4,5 +4,7 @@ from odoo.exceptions import UserError
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
+    company_name = fields.Char("My Company Name", default="ERPGO az");
+
     def print_custom_report(self):
         return self.env.ref("qweb_test.custom_header_footer_record").report_action(self)
