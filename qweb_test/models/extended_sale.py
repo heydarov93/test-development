ad3524668f405153b004d44fae6a7989e481cf86
yashar <yashar>
2025-10-24 14:52:11 +0000

    practice fields - selection

--- qweb_test/models/extended_sale.py
+++ qweb_test/models/extended_sale.py
@@ -7,9 +7,14 @@ class SaleOrder(models.Model):
     company_name = fields.Char("My Company Name", default="ERPGO az");
     custom_report = fields.Html("Create Custom Report");
     show_preview = fields.Boolean("Show Preview", default=False)
+    gender = fields.Selection([('male', 'Kişibala'),('female', 'Xanım')], default="male")
+    advanced_gender = fields.Selection("_get_advanced_genders", required=True)
 
     def print_custom_report(self):
         return self.env.ref("qweb_test.custom_header_footer_record").report_action(self)
 
     def toggle_preview(self):
-        self.show_preview = !(self.show_review);
+        self.show_preview = not self.show_preview;
+
+    def _get_advanced_genders(self):
+        return [('a', 'A'),('b', 'B'), ('c', 'C')];
