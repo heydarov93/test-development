818930d0ff34f7ef87b9e9e07bccd2a5b83ac386
yashar <yashar>
2025-10-24 12:42:14 +0000

    implement preview toggle for html field

--- qweb_test/models/extended_sale.py
+++ qweb_test/models/extended_sale.py
@@ -5,6 +5,11 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     company_name = fields.Char("My Company Name", default="ERPGO az");
+    custom_report = fields.Html("Create Custom Report");
+    show_preview = fields.Boolean("Show Preview", default=False)
 
     def print_custom_report(self):
         return self.env.ref("qweb_test.custom_header_footer_record").report_action(self)
+
+    def toggle_preview(self):
+        self.show_preview = !(self.show_review);
